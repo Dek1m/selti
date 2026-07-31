69f4550d60d6ae1b50dbd70bd0d4c468c5a92f74
Dek1m <s.eltsov@argenta.pro>
2026-07-31 20:41:45 +0000

    fix: импорт задач в __init__.py для autodiscover

--- memory_server/tasks/__init__.py
+++ memory_server/tasks/__init__.py
@@ -1,1 +1,6 @@
 # Celery tasks package
+# Импортируем все модули с задачами для autodiscover
+
+from memory_server.tasks import memory_tasks  # noqa: F401
+from memory_server.tasks import hash_tasks  # noqa: F401
+from memory_server.tasks import worker_stats  # noqa: F401
